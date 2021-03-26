d5f62b4ef2f0c60d3b2e1ac7f36a5277fb92d980
Mohammed Nagdy <mohammed.nagdy@itpeaks.com>
2021-03-26 11:55:13 +0200

    added logo in navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "../style.css";
 import Navbar from "./Navbar";
+import ideamarketLogo from "../assets/IdeaMarkets_Gradient-removebg-preview 1.png"
 
 class Header extends Component {
   state = {
@@ -18,8 +19,11 @@ class Header extends Component {
   handleScroll = () => {
     if (window.scrollY > 20) {
       document.querySelector(".bar").className = "p-absolute bar scroll";
+      document.querySelector(".ideamarket-name").className = "ideamarket-name scroll";
     } else {
       document.querySelector(".bar").className = "p-absolute bar";
+      document.querySelector(".ideamarket-name").className = "ideamarket-name";
+
     }
   };
 
@@ -32,6 +36,10 @@ class Header extends Component {
     ];
     return (
       <div className="p-absolute bar ">
+        <div className="navbar-brand idea-brand">
+          <img className="brand" src={ideamarketLogo} alt="ideamarket-logo" />
+          <span className="ideamarket-name">Ideamarket</span>
+        </div>
         <ul className="d-flex justify-content-center">
           {clickables.map((clickable, i) => {
             return (
